0ef8eb0362903e486bdf5f5e10d64582dc499b29
KoennerJonas <Jonas.Koenner@Student.HTW-Berlin.de>
2022-12-12 20:49:16 +0100

    'css'

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,21 +1,55 @@
 <template>
-<div>
-  <div class="font-primary font-extrabold pl-40 pt-5 ">
-    Meine Räume
-  </div>
-</div>
+  <body>
+  <Navbar></Navbar>
+    <div class="navbar">
+      <ul class="font-primary dont font-extrabold caret-amber-300 ">
+        <li>Meine Räume</li>
+        <li>stuff</li>
+        <li>stuff</li>
+      </ul>
+    </div>
+    <div class="content ">
+      <button class="font-primary font-extrabold bg-primary rounded-full h-14 w-40">RAUM ERSTELLEN</button>
+    </div>
+  </body>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
   name: 'StartView',
-  components: {}
+  components: {
+    Navbar
+  }
 
 }
 </script>
 
 
 <style scoped>
+.navbar{
+  width: 85%;
+  margin: auto;
+  padding:  23px 0 0 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.navbar ul li{
+  display: inline-block;
+  margin: 0 20px;
+  text-transform: uppercase;
+  position: relative;
+}
+.content{
+  width: 100%;
+  position: absolute;
+  top: 60%;
+  transform: translateY(-50%);
+  text-align: center;
+
+}
+
 
 
 
