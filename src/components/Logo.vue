afbd57b1ecb7dfe19c523718b5a016550fb54f64
David Seese <david-seese@outlook.com>
2023-01-01 15:02:45 +0100

    Logo hover

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" absolute w-full">
-    <div class=" flex justify-between ml-4 mt-4 " >
+  <div class=" absolute w-full ">
+    <div @click="this.$router.push('/')" class=" flex justify-between ml-4 mt-4 cursor-pointer" >
       <h1 class=" font-primary font-bold text-2xl text-black duration-200 hover:text-primary">Bringify</h1>
     </div>
   </div>
